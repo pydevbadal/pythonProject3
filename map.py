a7696eb4f90aa48657dcb3c9a19b9c0dde536cba
Badalsarkar12 <mdbadalsarkar87@gmail.com>
2024-04-13 19:09:18 +0600

    updated code

--- map.py
+++ map.py
@@ -1,6 +1,7 @@
-#Three lines to make our compiler able to draw:
+# Three lines to make our compiler able to draw:
 import sys
 import matplotlib
+
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
@@ -12,8 +13,6 @@ ypoints = np.array([3, 10])
 plt.plot(xpoints, ypoints)
 plt.show()
 
-#Two  lines to make our compiler able to draw:
+# Two  lines to make our compiler able to draw:
 plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
-
-
